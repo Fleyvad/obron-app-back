f057b81ce1394940eb0b4c45fb31089eda0183b9
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-22 19:37:09 +0100

    create projects controllers

---
+++ src/api/projects/projects-controllers.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from 'express';
+import { Project, ProjectModel } from './projects-schema.js';
+import { CustomHTTPError } from '../utils/errors/custom-http-error.js';
+export const createProjectController: RequestHandler<
+  unknown,
+  Project | { msg: string },
+  Project,
+  unknown
+> = async (req, res, next) => {
+  const { projectName } = req.body;
+
+  try {
+    if (!projectName) {
+      throw new CustomHTTPError(400, 'your project has no name');
+    }
+
+    const newProject: Project = { ...req.body };
+    const project = await ProjectModel.create(newProject);
+    return res.status(201).json(project);
+  } catch (error) {
+    next(error);
+  }
+};
