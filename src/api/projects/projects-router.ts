2de39a7404c7b5899f33fe022f9b9dd1f661d636
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-24 13:26:11 +0100

    add route to createProjectController with supabase and validations

--- src/api/projects/projects-router.ts
+++ src/api/projects/projects-router.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import { validate } from 'express-validation';
+import { authMiddleware } from '../auth/auth-middleware.js';
+import { projectValidation } from '../auth/auth-validation.js';
+import { upload } from './img-upload-middleware.js';
 import {
   createProjectController,
   getAllProjectsController,
 } from './projects-controllers.js';
+import { supabaseMiddleware } from './supabase-middleware.js';
 
 export const projectsRouter = express.Router();
 
-projectsRouter.route('/create').post(createProjectController);
+projectsRouter
+  .route('/create')
+  .post(
+    authMiddleware,
+    validate(projectValidation),
+    upload.single('upload'),
+    supabaseMiddleware,
+    createProjectController,
+  );
 
 projectsRouter.route('/').get(getAllProjectsController);
 
