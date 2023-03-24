a35dddb85f614aaedb15e57e4f524052ab627c18
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-24 13:11:11 +0100

    create supabase and multer middleware

---
+++ src/api/projects/supabase-middleware.ts
@@ -0,0 +1,38 @@
+import { RequestHandler } from 'express';
+import {
+  PROJECT_BUCKET_NAME,
+  supabase,
+} from '../../database/supabase-client.js';
+import log from '../../logger.js';
+
+export const supabaseMiddleware: RequestHandler<
+  unknown,
+  unknown,
+  unknown,
+  unknown,
+  { email: string; picture: string }
+> = async (req, res, next) => {
+  const { email } = res.locals;
+  const fileBuffer = req.file?.buffer;
+  let projectPicture;
+
+  if (fileBuffer !== undefined) {
+    const fileName = `${email}${Date.now()}`;
+    const { error } = await supabase.storage
+      .from(PROJECT_BUCKET_NAME)
+      .upload(fileName, fileBuffer);
+
+    if (error === null) {
+      const { data } = supabase.storage
+        .from(PROJECT_BUCKET_NAME)
+        .getPublicUrl(fileName);
+      projectPicture = data.publicUrl;
+      log.info('Public URL generated', data.publicUrl);
+
+      res.locals.picture = projectPicture;
+      log.info(res.locals.picture);
+    }
+  }
+
+  next();
+};
